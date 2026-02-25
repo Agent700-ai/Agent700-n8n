8982c664ebe5853e6ac5f80eadc8bb404fa9858a
Eric Marshall <eric.marshall@levelupdevelopment.com>
2026-02-25 15:57:12 -0500

    Startover

--- dist/nodes/Agent700Agent/Agent700Agent.node.d.ts
+++
@@ -1,6 +0,0 @@
-import { IExecuteFunctions, INodeExecutionData, INodeType, INodeTypeDescription } from 'n8n-workflow';
-export declare class Agent700Agent implements INodeType {
-    description: INodeTypeDescription;
-    execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]>;
-}
-export default Agent700Agent;
--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import { config } from '@n8n/node-cli/eslint';
-
-export default [
-	...config,
-	{
-		ignores: ['tests/**/*', 'jest.config.js', '**/*.test.ts', '**/*.spec.ts'],
-	},
-];
